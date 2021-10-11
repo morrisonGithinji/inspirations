6ff6869cd88d7dc0cc84726af955bef0b85423b7
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-11 19:57:14 +0300

    add create quote function

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  newQuote = new Quote("", "","",new Date());
+  @Output()createQuote = new EventEmitter<Quote>()
+
+  submitQuote(){
+    this.createQuote.emit(this.newQuote)
+  }
+
   constructor() { }
 
   ngOnInit(): void {
