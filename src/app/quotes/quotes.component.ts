b85a0565477596b37bc5903bc89daf4fc92eb8b5
morrisonGithinji <morrison.githinji@student.moringaschool.com>
2021-10-11 19:42:41 +0300

    add delete and create quote function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -15,6 +15,19 @@ export class QuotesComponent implements OnInit {
   
 
  ];
+      deleteQuote(toDelete, index){
+       if (toDelete) {
+        this.quotes.splice(index, 1);
+      }
+  }
+    createNewQuote(quote){
+      this.quotes.push(quote);
+
+    }
+    viewDetails(index){
+    this.quotes[index].showDetails =!this.quotes[index].showDetails;
+  }
+
 
 
   constructor() { }
